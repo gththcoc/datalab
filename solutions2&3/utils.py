66b2f46904656e538df3c03642bb53ccb6424211
sebastian2 <sebastian.gomez.lopez@aei.mpg.de>
2021-12-08 12:51:11 +0100

    excersice 3 solutions until roc curve

---
+++ solutions2&3/utils.py
@@ -0,0 +1,109 @@
+import os
+
+import sys
+import subprocess
+import uuid
+
+exe_dir = "../code" # directory where the executable is found
+output_dir = "../code/data_lab2" # directory to save the data file
+
+
+# Call generate source with given parameters
+# Have some default values
+def generate_source(
+    outputFile=None, 
+    sigma=0.0, 
+    delta_T=None, 
+    N=100000,
+    f=0, 
+    phi=0, 
+    A=0, 
+    r=0
+):
+    """
+    Usage: ./generate_source  [-h] [-o <file>] [-s <float>] [-d <float>] [-N <int>] [-f <float>] [-p <float>] [-a <float>] [-r <int>]
+      -o, --output=<file>       output file
+      -s, --sigma=<float>       standard deviation sigma
+      -d, --deltaT=<float>      delta T betwean measurements
+      -N, --length=<int>        number of elements
+      -f, --freq=<float>        frequency
+      -p, --phase=<float>       phase
+      -a, --amplitude=<float>   signal amplitude
+      -r, --random=<int>        if set to 1, generate random signal
+      -h, --help                print this help and exit
+    """
+    
+    if delta_T == None : 
+        delta_T = 1/N
+    if outputFile == None:
+        outputFile = "data_" + str(uuid.uuid4()) + ".csv"
+    
+    outputPath = os.path.join(output_dir, outputFile) # output_dir + "/" + "myfilename.csv"
+    exe_path = os.path.join(exe_dir, 'generate_source')
+    cmd_str = f"{exe_path} -o {outputPath} -s {sigma} "\
+        f"-d {delta_T} -N {N} -f {f} -p {phi} -a {A} -r {r}"
+    
+    # Optionally print the command line and test it outside the notebook
+    #print(cmd_str)
+    
+    cmd = cmd_str.split(' ')
+    process = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    if err:
+        print('The process raised an error:', err.decode())
+    if out:
+        print('The process has an output:', out.decode())
+
+    return os.path.join(output_dir, outputFile)
+
+
+
+
+
+def prober(inputFile, f=0, outputFile=None, phi=0, templateFile=None, mod=1):
+    """
+    Usage: ./prober  [-h] [-i <file>] [-o <file>] [-f <float>] [-p <float>] [-t <file>] [-m <int>]
+      -i, --input=<file>        input file
+      -o, --output=<file>       output file
+      -f, --freq=<float>        frequency
+      -p, --phase=<float>       phase
+      -h, --help                print this help and exit
+      -t, --templatebank=<file> template bank file
+      -m, --method=<int>        select method to analyze signal 1, 2 or 3
+    """
+    
+    inputFile = os.path.basename(inputFile)
+    
+    if outputFile == None:
+        outputFile = "results_" + str(uuid.uuid4()) + ".csv"
+    
+    exe_path = os.path.join(exe_dir, 'prober')
+    
+    """
+    prober -i data/data_01.csv -o data/results_01.csv -t template.txt -m 1
+    """
+    cmd_str = f"{exe_path} -i {output_dir}/{inputFile} "\
+        f"-o {output_dir}/{outputFile} -m {mod}"
+    #print(cmd_str)
+    if templateFile:
+        cmd_str += f" -t {output_dir}/{templateFile} "
+    else:
+        cmd_str += f" -f {f} -p {phi}"
+    
+    # Optionally print the command line and test it outside the notebook
+    # print(cmd_str)
+    
+    cmd = cmd_str.split(' ')
+    process = subprocess.Popen(cmd,stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    if err:
+        print('The process raised an error:', err.decode())
+    if out:
+        #print('The process has an output:', out.decode())
+        return out.decode().split(' ')
+    if os.path.isfile(outputFile):
+        print(f"Output generated in file {outputFile}")
+
+    return os.path.join(output_dir, outputFile)
+    
+
